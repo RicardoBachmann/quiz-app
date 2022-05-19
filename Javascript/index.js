7ecd8ef96fdc4e63065f0b763069cd836fb20a64
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2022-05-19 14:13:46 +0200

    Add linting

--- Javascript/index.js
+++ Javascript/index.js
@@ -4,7 +4,7 @@ const showButton = document.querySelector('[data-js="action-button"]');
 const showAnswerText = document.querySelector('[data-js="show-answer-text"]');
 
 function onButtonClick() {
-  showAnswerText.classList.add("show-answer--active");
+  showAnswerText.classList.add('show-answer--active');
 }
 
-showButton.addEventListener("click", onButtonClick);
+showButton.addEventListener('click', onButtonClick);
