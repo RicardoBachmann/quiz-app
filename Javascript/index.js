7cd0f14d8a563a4502e7ab4ea19af42419a42eb1
Ricardo Bachmann <ricardo.bachmann@icloud.com>
2022-05-19 08:22:59 +0200

    edit

--- Javascript/index.js
+++ Javascript/index.js
@@ -2,7 +2,6 @@ console.clear();
 
 const showButton = document.querySelector('[data-js="action-button"]');
 const showAnswerText = document.querySelector('[data-js="show-answer-text"]');
-console.log(showButton);
 
 function onButtonClick() {
   showAnswerText.classList.add("show-answer--active");
